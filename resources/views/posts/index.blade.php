13f0d3ca9fe11bd3fff6d6dc9f1a0a9c7bc4cce5
izumi <kizumi916@gmail.com>
2020-04-24 20:25:49 +0900

    cerate,edit機能

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,16 @@ Blog Posts
 @endsection
 
 @section('content')
-<h1>Blog Posts</h1>
+<h1>
+<a href="{{ url('/posts/create') }}" class="header_menu">新規作成</a>
+ブログ投稿
+</h1>
 <ul>
   @forelse ($posts as $post)
-    <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
+    <li>
+      <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
+      <a href="{{ action('PostsController@edit', $post) }}" class="edit">[編集]</a>
+    </li>
   @empty
   <li>No posts yet</li>
   @endforelse
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/{post}', 'PostsController@show')->where('post', '[0-9]+');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}/edit', 'PostsController@edit');
+Route::patch('/posts/{post}', 'PostsController@update');
+
+
 
