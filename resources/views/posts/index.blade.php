d33159b8ea8a62684de9a2a58825fb636429eedb
izumi <kizumi916@gmail.com>
2020-04-24 20:51:13 +0900

    削除機能

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,10 +14,16 @@ Blog Posts
     <li>
       <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
       <a href="{{ action('PostsController@edit', $post) }}" class="edit">[編集]</a>
+      <a href="#" class="del" data-id="{{ $post->id }}">[×]</a>
+      <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+        {{ csrf_field() }}
+        {{ method_field('delete') }}
+      </form>
     </li>
   @empty
   <li>No posts yet</li>
   @endforelse
 </ul>
+<script src="/js/main.js"></script>
 @endsection
 
