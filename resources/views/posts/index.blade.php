d248176e81024948b37d8af648d12baa1a966d49
izumi <kizumi916@gmail.com>
2020-04-25 14:16:40 +0900

    コメント機能

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,17 +11,17 @@ Blog Posts
 </h1>
 <ul>
   @forelse ($posts as $post)
-    <li>
-      <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
-      <a href="{{ action('PostsController@edit', $post) }}" class="edit">[編集]</a>
-      <a href="#" class="del" data-id="{{ $post->id }}">[×]</a>
-      <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
-        {{ csrf_field() }}
-        {{ method_field('delete') }}
-      </form>
-    </li>
+  <li>
+    <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
+    <a href="{{ action('PostsController@edit', $post) }}" class="edit">[編集]</a>
+    <a href="#" class="del" data-id="{{ $post->id }}">[×]</a>
+    <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+      {{ csrf_field() }}
+      {{ method_field('delete') }}
+    </form>
+  </li>
   @empty
-  <li>No posts yet</li>
+  <li>投稿がありません</li>
   @endforelse
 </ul>
 <script src="/js/main.js"></script>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('title')
-$post->title
+{{$post->title}}
 @endsection
 
 @section('content')
@@ -10,4 +10,33 @@ $post->title
   {{ $post->title }}
 </h1>
 <p>{!! nl2br(e($post->body)) !!}</p>
+
+<h2>コメント</h2>
+<ul>
+  @forelse ($post->comments as $comment)
+  <li>
+    {{ $comment->body }}
+    <a href="#" class="del" data-id="{{ $comment->id }}">[×]</a>
+    <form method="post" action="{{ action('CommentsController@destroy', [$post, $comment]) }}" id="form_{{ $comment->id }}">
+      {{ csrf_field() }}
+      {{ method_field('delete') }}
+    </form>
+  </li>
+  @empty
+  <li>コメントはありません</li>
+  @endforelse
+</ul>
+<form method="post" action="{{ action('CommentsController@store', $post) }}">
+  {{ csrf_field() }}
+  <p>
+    <input type="text" name="body" placeholder="コメントを入力してください" value="{{ old('body') }}">
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+    @endif
+  </p>
+  <p>
+    <input type="submit" value="コメントを投稿">
+  </p>
+</form>
+<script src="/js/main.js"></script>
 @endsection
